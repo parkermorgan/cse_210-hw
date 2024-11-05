ddbd8df922cab5a2a6289f68bf84ba026eb6513b
parkermorgan <156543039+parkermorgan@users.noreply.github.com>
2024-11-04 20:09:30 -0700

    Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1, 101);
+
+        string response = "yes";
+
+        while (response == "yes")
+        {
+            Console.Write("What is your guess? ");
+            string guessString = Console.ReadLine();
+            int guessNum = int.Parse(guessString);
+
+            if (guessNum > magicNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guessNum < magicNum)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guessNum == magicNum)
+            {
+                Console.WriteLine("You guessed it!");
+
+            }
+        }
+
     }
 }
