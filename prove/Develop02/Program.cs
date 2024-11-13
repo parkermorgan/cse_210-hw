23af31ce87282d9d87403884ad4fc5475a8d296e
parkermorgan <156543039+parkermorgan@users.noreply.github.com>
2024-11-13 12:07:28 -0700

    Created stubs for journal

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
+        Journal theJournal = new Journal();
+        Entry anEntry = new Entry();
+
         Console.WriteLine("Hello Develop02 World!");
     }
 }
